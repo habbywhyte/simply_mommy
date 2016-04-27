7055c4c20cbbeefd39ddbc0f3de5b4212f541e37
Habby Whyte <bahybaj@hotmail.com>
2016-04-27 15:01:24 -0400

    fixes comment form submit

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,6 @@
 class CommentsController < ApplicationController
   def create
-    @post = Post.find(params[:id])
+    @post = Post.find(params[:post_id])
     @comment = @post.comments.create(params[:comment].permit(:name, :body))
 
     redirect_to post_path(@post)
