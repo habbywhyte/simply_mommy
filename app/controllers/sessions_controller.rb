330eb5501d748a7b9c6b021e720f6cdc3924709d
Habby Whyte <bahybaj@hotmail.com>
2016-04-28 11:27:10 -0400

    fixes logout destroy action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,6 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(email: params[:signin][:email])
-    puts user.inspect
     if user
       if user.authenticate(params[:signin][:password])
         session[:user_id] = user.id
@@ -18,13 +17,14 @@ class SessionsController < ApplicationController
       render :new
       return
     end
+  end
 
   def destroy
     session[:user_id] = nil
-    flash[:success] = "Signed out!"
-    redirect_to posts_path
+
+    flash[:success] = "You have signed out!"
+    redirect_to root_path
 
   end
 
-end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,8 @@ Rails.application.routes.draw do
 
   root 'pages#home', id: 'home'
 
+  # resources :sessions
+
 end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
